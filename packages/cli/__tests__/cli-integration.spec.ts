37ec96a5b85409f750fa89b378b13c11cc24be64
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2020-09-08 12:11:05 -0300

    chore: fix cli tests

--- packages/cli/__tests__/cli-integration.spec.ts
+++ packages/cli/__tests__/cli-integration.spec.ts
@@ -42,7 +42,8 @@ describe('kraken.js', () => {
     }, timeout);
 
     test('should generate .kraken/serverless.json file', async () => {
-      await cli('serverless print');
+      const output = await cli('serverless print --stage test');
+      console.debug(output);
       expect(filesystem.read('.kraken/serverless.json')).toMatchSnapshot();
     }, timeout);
 
