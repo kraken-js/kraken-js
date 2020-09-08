394382a1966195368ce7857370b655778485b1f6
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2020-09-08 18:12:46 -0300

    fix: resolve Query.SystemInfo

--- packages/aws/src/schema.ts
+++ packages/aws/src/schema.ts
@@ -1,9 +1,13 @@
+import { IExecutableSchemaDefinition } from '@graphql-tools/schema';
 // @ts-ignore
 import * as typeDefs from './schema.graphql';
 
-export const graphqlSchema = {
+export const graphqlSchema: IExecutableSchemaDefinition = {
   typeDefs,
   resolvers: {
+    Query: {
+      systemInfo: () => ({})
+    },
     SystemInfo: {
       region: () => process.env.AWS_REGION
     }
