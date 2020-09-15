753f08371195cf3d4552bed2fda1e75f58e16c16
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2020-09-15 10:58:45 -0300

    optimize jest runs

--- packages/jest/src/jest.config.js
+++ packages/jest/src/jest.config.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 require('dotenv').config({ path: '__tests__/.env.test' });
 
 module.exports = (dirname) => {
-  const packageJson = require(dirname + '/package.json');
   const setupFile = path.join(dirname, 'jest.setup.js');
   const setupFiles = fs.existsSync(setupFile) ? [setupFile] : undefined;
-  return ({
+  return {
     preset: 'ts-jest',
     testEnvironment: 'node',
     restoreMocks: true,
@@ -21,9 +20,6 @@ module.exports = (dirname) => {
     transform: {
       '\\.(gql|graphql)$': 'jest-transform-graphql',
       '\\.yml$': 'jest-yaml-transform'
-    },
-    moduleNameMapper: {
-      [packageJson.name]: '<rootDir>'
     }
-  });
+  };
 };
