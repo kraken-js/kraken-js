bfdea62ea9ecb69f529f88883d386b40a0c2ece4
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2020-09-23 00:51:49 +0100

    fix: serverless deployment issues (#36)
    * fix: serverless deployment issues
    
    * yarn upgrade

--- packages/demo/webpack.config.js
+++ packages/demo/webpack.config.js
@@ -1,7 +1,1 @@
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const webpack = require('@kraken.js/webpack').forServerless(__dirname);
-
-module.exports = {
-  ...webpack
-  // plugins: webpack.plugins.concat(new BundleAnalyzerPlugin())
-};
+module.exports = require('@kraken.js/webpack').forServerless(__dirname);
