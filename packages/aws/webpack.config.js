a13b1f0c0e0ff96e324e6ada4277be2208656795
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2020-10-20 22:29:52 -0300

    drop size-limit to enable webpack upgrade

--- packages/aws/webpack.config.js
+++ packages/aws/webpack.config.js
@@ -1,1 +1,3 @@
-module.exports = require('@kraken.js/webpack').forModule(__dirname, 'index.ts');
+module.exports = require('@kraken.js/webpack').forModule(__dirname, 'index.ts', {
+  performance: { maxAssetSize: 90 * 1024 }
+});
