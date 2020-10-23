26210fa0f2f6c38f83ebcfb736eb0517d73def28
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2020-10-23 09:33:46 -0300

    fix tests

--- packages/core/src/directives/publish-directive.ts
+++ packages/core/src/directives/publish-directive.ts
@@ -1,15 +1,18 @@
 import { SchemaDirectiveVisitor } from '@graphql-tools/utils';
-import { defaultFieldResolver } from 'graphql';
+import { defaultFieldResolver, isObjectType } from 'graphql';
 
 export class PublishDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field) {
     const { resolve = defaultFieldResolver } = field;
+    const typeName = field.type?.name;
     const triggerNames = this.args.triggerNames;
+    const isObject = isObjectType(field.type);
 
     field.resolve = async function(source, args, context: Kraken.ExecutionContext, info) {
       const result = await resolve.apply(this, [source, args, context, info]);
+      const payload = isObject ? { __typename: typeName, ...result } : result;
       for (const triggerName of triggerNames) {
-        await context.$pubsub.publish(triggerName, result);
+        await context.$pubsub.publish(triggerName, payload);
       }
       return result;
     };
