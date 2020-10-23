2baa2a61037b31e7e52ca8c7e7428104241b53e0
Marcelo Luiz Onhate <onhate@users.noreply.github.com>
2020-10-23 11:53:12 -0300

    codecov

---
+++ packages/aws/__tests__/schemas/core/resolvers/aws-dynamo-set-scalar.spec.ts
@@ -0,0 +1,23 @@
+import { DynamoDbSet } from '@kraken.js/aws/src/schemas/core/resolvers/aws-dynamo-set-scalar';
+import DynamoDBSet from 'aws-sdk/lib/dynamodb/set';
+
+describe('AWS DynamoDB Set Scalar', () => {
+  it.each([
+    [new DynamoDBSet([1, 2]), [1, 2]],
+    [[1, 2], [1, 2]],
+    ['str', null],
+    [null, null]
+  ])('should serialize %o to %o', (input, expected) => {
+    const actual = DynamoDbSet.serialize(input);
+    expect(actual).toEqual(expected);
+  });
+
+  it.each([
+    [[1, 2], new DynamoDBSet([1, 2])],
+    ['str', 'str'],
+    [null, null]
+  ])('should parseValue %o to %o', (input, expected) => {
+    const actual = DynamoDbSet.parseValue(input);
+    expect(actual).toEqual(expected);
+  });
+});
